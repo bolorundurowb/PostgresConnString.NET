dcde50555da2eba1aabe303e54a2f5083fee9fe4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-26 22:51:49 +0100

    ft: add in model

---
+++ PostgresConnString.NET/Models/ConnectionDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace PostgresConnString.NET.Models
+{
+    public class ConnectionDetails
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string Database { get; set; }
+
+        public string User { get; set; }
+
+        public string Password { get; set; }
+    }
+}
