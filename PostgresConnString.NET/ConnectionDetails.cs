08a5ddd2c91085e11e58e79f11b82611e5a41810
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-26 22:55:45 +0100

    ft: add in constructor

---
+++ PostgresConnString.NET/ConnectionDetails.cs
@@ -0,0 +1,24 @@
+﻿namespace PostgresConnString.NET
+{
+    public class ConnectionDetails
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string Database { get; set; }
+
+        public string User { get; set; }
+
+        public string Password { get; set; }
+
+        public ConnectionDetails(string host, string user, string password, string database, int port = 5432)
+        {
+            Host = host;
+            User = user;
+            Password = password;
+            Database = database;
+            Port = port;
+        }
+    }
+}
