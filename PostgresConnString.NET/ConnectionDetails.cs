dfee148e0693e8210e82da39d459049732a3d231
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-26 23:49:28 +0100

    ft: set propert values

--- PostgresConnString.NET/ConnectionDetails.cs
+++ PostgresConnString.NET/ConnectionDetails.cs
@@ -5,23 +5,46 @@ namespace PostgresConnString.NET
 {
     public class ConnectionDetails
     {
-        public string Host { get; set; }
+        public string Host { get; set; } = string.Empty;
 
-        public int Port { get; set; }
+        public int Port { get; set; } = 5432;
 
-        public string Database { get; set; }
+        public string Database { get; set; } = string.Empty;
 
-        public string User { get; set; }
+        public string User { get; set; } = string.Empty;
 
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
+
+        public ConnectionDetails()
+        {
+        }
 
         public ConnectionDetails(string host, string user, string password, string database, int? port = null)
         {
-            Host = host;
-            User = user;
-            Password = password;
-            Database = database;
-            Port = port ?? 5432;
+            if (!string.IsNullOrWhiteSpace(host))
+            {
+                Host = host;
+            }
+
+            if (!string.IsNullOrWhiteSpace(user))
+            {
+                User = user;
+            }
+
+            if (!string.IsNullOrWhiteSpace(password))
+            {
+                Password = password;
+            }
+
+            if (!string.IsNullOrWhiteSpace(database))
+            {
+                Database = database;
+            }
+
+            if (port.HasValue)
+            {
+                Port = port.Value;
+            }
         }
 
         public static ConnectionDetails Parse(string url)
