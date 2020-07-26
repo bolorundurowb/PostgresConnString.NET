15253f93dcb400d1b0fca22585ce4ad6422446a9
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-26 23:58:23 +0100

    ft: test non-default constructor

---
+++ PostgresConnString.NET.Tests/ConnectionDetails.Tests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PostgresConnString.NET.Tests
+{
+    public class ConnectionDetailsTests
+    {
+        [Fact]
+        public void DefaultConstructorShouldUseDefaultValue()
+        {
+            var details = new ConnectionDetails();
+
+            details
+                .Should()
+                .NotBeNull();
+            details.Database
+                .Should()
+                .BeEmpty();
+            details.User
+                .Should()
+                .BeEmpty();
+            details.Password
+                .Should()
+                .BeEmpty();
+            details.Host
+                .Should()
+                .BeEmpty();
+            details.Port
+                .Should()
+                .Be(5432);
+        }
+        
+        [Fact]
+        public void OverloadConstructorShouldUseDefaultValue()
+        {
+            var host = "localhost";
+            var user = "user";
+            var password = "password";
+            var port = 3310;
+            var database = "database";
+            
+            var details = new ConnectionDetails(host, user, password, database, port);
+
+            details
+                .Should()
+                .NotBeNull();
+            details.Database
+                .Should()
+                .Be(database);
+            details.User
+                .Should()
+                .Be(user);
+            details.Password
+                .Should()
+                .Be(password);
+            details.Host
+                .Should()
+                .Be(host);
+            details.Port
+                .Should()
+                .Be(port);
+        }
+    }
+}
