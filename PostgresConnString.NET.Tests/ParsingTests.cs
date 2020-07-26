f5ce9cf4e5a4d5854b048b1a6db9bda8348a0f25
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-27 00:44:39 +0100

    ft: add error mode parser tests

--- PostgresConnString.NET.Tests/ParsingTests.cs
+++ PostgresConnString.NET.Tests/ParsingTests.cs
@@ -1,7 +1,36 @@
-﻿namespace PostgresConnString.NET.Tests
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PostgresConnString.NET.Tests
 {
     public class ParsingTests
     {
+        [Fact]
+        public void ShouldThrowOnNullInput()
+        {
+            Action action = () => ConnectionDetails.Parse(null);
+            action
+                .Should()
+                .ThrowExactly<ArgumentNullException>();
+        }
         
+        [Fact]
+        public void ShouldThrowOnEmptyInput()
+        {
+            Action action = () => ConnectionDetails.Parse(string.Empty);
+            action
+                .Should()
+                .ThrowExactly<ArgumentException>();
+        }
+        
+        [Fact]
+        public void ShouldThrowOnWhitespaceInput()
+        {
+            Action action = () => ConnectionDetails.Parse("  \t \r \n");
+            action
+                .Should()
+                .ThrowExactly<ArgumentException>();
+        }
     }
 }
