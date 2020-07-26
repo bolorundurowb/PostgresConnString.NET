da301aeb5fbf47202e5f27149ccb518ea15624ef
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-26 23:33:07 +0100

    ft: add in test project

--- PostgresConnString.NET/Utils/Parser.cs
+++ PostgresConnString.NET/Utils/Parser.cs
@@ -1,10 +1,12 @@
-﻿namespace PostgresConnString.NET.Utils
+﻿using System;
+
+namespace PostgresConnString.NET.Utils
 {
     internal static class Parser
     {
         internal static (string, string, string, string, int?) Parse(string url)
         {
-            
+            throw new NotImplementedException();
         }
     }
 }
