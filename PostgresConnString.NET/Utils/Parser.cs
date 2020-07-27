3fab38e6fe1cffec8af0cca3c7b47f3f600cfa50
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-27 01:12:10 +0100

    ft: add support for unix sockets

--- PostgresConnString.NET/Utils/Parser.cs
+++ PostgresConnString.NET/Utils/Parser.cs
@@ -6,7 +6,18 @@ namespace PostgresConnString.NET.Utils
     {
         internal static (string, string, string, string, int?) Parse(string url)
         {
-            throw new NotImplementedException();
+            var normalizedUrl = url.Trim();
+
+            if (normalizedUrl.StartsWith("/"))
+            {
+                return ParseUnixSocket(normalizedUrl);
+            }
+        }
+
+        private static (string, string, string, string, int?) ParseUnixSocket(string socket)
+        {
+            var parts = socket.Split(' ');
+            return (parts[0], null, null, parts[1], null);
         }
     }
 }
