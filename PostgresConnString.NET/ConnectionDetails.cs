d843c122780a535fdcb04730cedd3f766ab9036e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-07-27 01:00:38 +0100

    ft: add some doc

--- PostgresConnString.NET/ConnectionDetails.cs
+++ PostgresConnString.NET/ConnectionDetails.cs
@@ -3,25 +3,56 @@ using PostgresConnString.NET.Utils;
 
 namespace PostgresConnString.NET
 {
+    /// <summary>
+    /// Contains a postgresql connection details
+    /// </summary>
     public class ConnectionDetails
     {
+        /// <summary>
+        /// The database server host address
+        /// Default: ""
+        /// </summary>
         public string Host { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The port the database is on
+        /// Default: 5432
+        /// </summary>
         public int Port { get; set; } = 5432;
 
+        /// <summary>
+        /// The database name
+        /// Default: ""
+        /// </summary>
         public string Database { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The login user name
+        /// Default: ""
+        /// </summary>
         public string User { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The login password
+        /// Default: ""
+        /// </summary>
         public string Password { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Initialize with default values
         /// </summary>
         public ConnectionDetails()
         {
         }
 
+        /// <summary>
+        /// Initialize with specified values
+        /// </summary>
+        /// <param name="host">The database server host address</param>
+        /// <param name="user">The login user name</param>
+        /// <param name="password">The login password</param>
+        /// <param name="database">The database name</param>
+        /// <param name="port">The port the database is on</param>
         public ConnectionDetails(string host, string user, string password, string database, int? port = null)
         {
             if (!string.IsNullOrWhiteSpace(host))
